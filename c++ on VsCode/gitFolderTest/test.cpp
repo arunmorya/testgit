1fdb7d9e1b477ed896d2f79b95b7810a04f332c2
ARUN KUMAR <krarun10199@gmail.com>
2020-12-01 10:42:18 +0530

    new folder

---
+++ c++ on VsCode/gitFolderTest/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// Driver Code 
+int main() 
+{ 
+    int** dp;
+    dp=new int*[10];
+    dp[0]=new int[50];
+    cout<<sizeof(dp);
+    cout<<"you are inside the folder ";
+}
